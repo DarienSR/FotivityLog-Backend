07088f901d31d97ca7f5f7af3534ade66455500f
DarienSR <dariensloan@gmail.com>
2022-12-21 14:53:05 -0500

    Loaded live data with heatmap. Needs proper color adjustment / scale

--- webapp/src/components/Graphs.js
+++ webapp/src/components/Graphs.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import CalendarHeatmap from 'react-calendar-heatmap'; // https://www.npmjs.com/package/react-calendar-heatmap
 import Line from './charts/Line';
 import Heatmap from "./charts/Heatmap";
 import Treemap from './charts/Treemap';
@@ -10,7 +9,7 @@ export default function Graphs() {
   let [refreshToken, setRefreshToken]= useState(false);
   let [data, setData] = useState([]);
 
-  let getMinutes = function(start, end) { return Math.abs(start  - end) / 1000 / 60 / 60};
+  let getMinutes = function(start, end) { return Math.floor(((Math.abs(start  - end)) / 1000) / 60); } // hours
 
 
   
@@ -18,14 +17,18 @@ export default function Graphs() {
   useEffect(() => {
     let url = `${process.env.REACT_APP_URL}session/all`;
     axios.get(url).then(function(response) {
-      let topics = [], startDates = [], session_lengths = [];
+      let topics = [], session_times = [];
       response.data.forEach((obj) => {
         topics.push(obj.topic.toUpperCase())
-        startDates.push(obj.start_time.split('T')[0])    
-        session_lengths.push(getMinutes(new Date(obj.start_time), new Date(obj.end_time)))
+        
+        session_times.push({
+          date: obj.start_time.split('T')[0],
+          count: getMinutes(new Date(obj.start_time), new Date(obj.end_time))
+        })
+        
       })
 
-      setData({ topics, startDates, session_lengths });
+      setData({ topics, session_times });
       setSessions(response.data); // [0] is current Session
     }).catch(function(error) {
       console.log(error)
@@ -39,8 +42,8 @@ export default function Graphs() {
       <h1>Dashboard</h1>
       <Treemap data={data} />
       <Line sessions={sessions} data={ data } />
-      <div style={{ width: '30%', backgroundColor: 'red', padding: '1rem'}}>
-      <Heatmap />
+      <div style={{ width: '30%', padding: '1rem'}}>
+      <Heatmap sessions={ data.session_times }  />
       </div>
     </>
   )
--- webapp/src/components/charts/Heatmap.js
+++ webapp/src/components/charts/Heatmap.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import CalendarHeatmap from 'react-calendar-heatmap';
+import CalendarHeatmap from 'react-calendar-heatmap'; // https://www.npmjs.com/package/react-calendar-heatmap
 import 'react-calendar-heatmap/dist/styles.css';
-
+import "../../App.css"
 
 const today = new Date();
 
-export default function Heatmap() {
-  const randomValues = getRange(200).map(index => {
-    return {
-      date: shiftDate(today, -index),
-      count: getRandomInt(1, 3),
-    };
-  });
+export default function Heatmap(props) {
+  if(props.sessions === undefined) return null;
   return (
     <div>
-      <h1>6 Month History of Session Hours</h1>
+      <h1>6 Month History of Session Minutes</h1>
       <CalendarHeatmap
-        id="2"
         startDate={shiftDate(today, -200)}
         endDate={today}
-        values={randomValues}
+        values={props.sessions}
         classForValue={value => {
           if (!value) {
             return 'color-empty';
           }
-          return `color-github-${value.count}`;
+          if(value.count < 30) return 'color-scale-1'
+          if(value.count < 60) return 'color-scale-2'
+          if(value.count < 90) return 'color-scale-3'
+          if(value.count < 120) return 'color-scale-4'
+          else return 'red'
         }}
-        titleForValue={(value) => `${value.date.toISOString().slice(0, 10)} - ${
-          value.count
-        } hours`}
+        titleForValue={(value) => {
+          if(value === null) return null;
+          return `${ value.date } - ${ value.count }`
+        }
+        }
 
         showWeekdayLabels={true}
         onClick={value => alert(`Clicked on value with count: ${value.count}`)}
@@ -44,10 +44,3 @@ function shiftDate(date, numDays) {
   return newDate;
 }
 
-function getRange(count) {
-  return Array.from({ length: count }, (_, i) => i);
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
