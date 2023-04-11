a694fb8d474ecda8ddea799b4cbf788aa69e5d6d
DarienSR <dariensloan@gmail.com>
2023-04-11 14:15:28 -0400

    moved out backend

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 app.use(logger)
 
-// use it before all route definitions
-app.use(cors({origin: 'https://fotivitylog.com'}));
-app.use(
-  cors({origin: ['https://fotivitylog.com']})
-);
-
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
--- frontend/src/config/roles.js
+++
@@ -1,5 +0,0 @@
-export const ROLES = {
-  UserFree: 'UserFree',
-  UserPaid: 'UserPaid',
-  Admin: 'Admin'
-}
