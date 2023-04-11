854c3e60f81f493c4acdf40e677ffdd7ccbc0091
DarienSR <dariensloan@gmail.com>
2023-04-11 15:29:50 -0400

    change port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 8080
 
 connectDB()
 app.use(logger)
