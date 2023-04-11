cebf18fe15bb9e902556807fa69c8a41520518da
DarienSR <dariensloan@gmail.com>
2023-04-11 13:11:35 -0400

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 5000
 
 connectDB()
 app.use(logger)
-// Enable preflight requests for all routes
-app.options('*', cors(corsOptions));
+
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
@@ -28,14 +27,25 @@ app.use('/sessions', require('./routes/sessionRoutes'))
 
 
 app.all('*', (req, res) => {
-  res.status(404)
   if(req.accepts('html')) 
     res.sendFile(path.join(__dirname, 'views', '404.html'))
   else if (req.accepts('json'))
     res.json({ message: '404 Not Found' })
   else 
     res.type('txt').send('404 Not Found')
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 })
+//enables cors
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 app.use(errorHandler)
 mongoose.connection.once('open', () => {
